7851e7e6b94be89b12ac2d04ee982cfd7b75d5a9
s-webs <swebs.sh@gmail.com>
2022-06-27 17:26:46 +0600

    Установил Vue Закончил на 14:00 минуте

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <button @click="removeLike" style="margin-right: 10px">-</button>
+    <span>Likes: {{ likes }}</span>
+    <button @click="addLike" style="margin-left: 10px">+</button>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    removeLike() {
+      if (this.likes > 0) this.likes -= 1
+      else console.log('Отрицательного количества не может быть')
     }
   }
 }
+</script>
+
+<style scoped>
+
 </style>
